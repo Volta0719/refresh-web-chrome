167bdfbedfc7d9533ca51fb0688452dbe52cda99
fan <fan24min@qq.com>
2023-07-28 15:06:20 +0800

    💫 增加长久刷新的功能 (#15)

--- bg/store.js
+++ bg/store.js
@@ -1,4 +1,4 @@
-// let taskInfoList = {};
+// importScripts("../utils/tools.js")
 console.log('chrome bg', chrome)
 const getTaskList = () => {
     return new Promise((resolve, reject) => {
@@ -8,34 +8,6 @@ const getTaskList = () => {
     })
 }
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-    // if (request?.from === 'popup') {
-    //     if (request?.type === 'get') {
-    //         sendResponse({
-    //             taskInfoList,
-    //             message: 'ok'
-    //         })
-    //     } else if (request?.type === 'add') {
-    //         const addData = request?.addData;
-    //         taskInfoList[addData.id] = { ...addData };
-    //         sendResponse({
-    //             taskInfoList,
-    //             message: 'ok'
-    //         })
-    //     } else if (request?.type === 'delete') {
-    //         if (taskInfoList.hasOwnProperty(request?.id)) {
-    //             delete taskInfoList[request.id]
-    //             sendResponse({
-    //                 message: 'ok'
-    //             })
-    //         } else {
-    //             sendResponse({
-    //                 message: `taskInfoList has no id(${request?.id})`
-    //             })
-    //         }
-
-    //     }
-    // } 
-
     if (request?.from === 'content') {
         //保留  然后通过存到session
         const { tab } = sender;
@@ -45,39 +17,53 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
                 taskInfoList[tab.id].nexttime = request?.nextTime;
                 taskInfoList[tab.id].count = (+taskInfoList[tab.id].count) + 1;
             } else if (request?.type === 'stop') {
+                if (taskInfoList[tab.id].refreshType === 'alarms') {
+                    await chrome.alarms.clear(`${tab.id}`);
+                }
                 delete taskInfoList[tab.id]
             }
-
-            chrome.storage.session.set({ vlotaTaskList: { ...taskInfoList } })
+            await chrome.storage.session.set({ vlotaTaskList: { ...taskInfoList } })
             sendResponse({
+                from: 'bg',
                 message: 'ok'
             })
         } else {
             sendResponse({
+                from: 'bg',
                 message: `[${request?.type}]TaskList Has Not Own Property ${tab.id}`
             })
         }
-
     }
 }
 );
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+    chrome.tabs.reload(+alarm.name, { bypassCache: false })
+})
+
 chrome.tabs.onRemoved.addListener(async (tabId, windowData) => {
     if (!windowData.isWindowClosing) {
         //windowData:{isWindowClosing:false,windowId:11222}
         const taskInfoList = await getTaskList()
         if (taskInfoList.hasOwnProperty(tabId)) {
+            if (taskInfoList[tabId].refreshType === 'alarms') {
+                await chrome.alarms.clear(`${tabId}`);
+            }
             delete taskInfoList[tabId]
-            chrome.storage.session.set({ vlotaTaskList: { ...taskInfoList } })
+            await chrome.storage.session.set({ vlotaTaskList: { ...taskInfoList } })
         }
     }
 })
 chrome.windows.onRemoved.addListener(async (winid) => {
     const taskInfoList = await getTaskList()
     const tabsIdList = Object.keys(taskInfoList);
-    tabsIdList.forEach(id => {
+    tabsIdList.forEach(async (id) => {
         if (taskInfoList[id].winId == winid) {
+            if (taskInfoList[id].refreshType === 'alarms') {
+                await chrome.alarms.clear(`${id}`);
+            }
             delete taskInfoList[id]
         }
     })
-    chrome.storage.session.set({ vlotaTaskList: { ...taskInfoList } })
+    await chrome.storage.session.set({ vlotaTaskList: { ...taskInfoList } })
 })
